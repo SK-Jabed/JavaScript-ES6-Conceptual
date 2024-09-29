3d575899071eee317029a1360a31d6e7e4cecfcc
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-29 23:42:28 +0600

    Arrow JS File Completed

--- arrow.js
+++ arrow.js
@@ -1,12 +1,38 @@
 // Normal Function
-// function myName() {
-//     return "Sheikh Jabed";
-// }
+function myName() {
+    return "Sheikh Jabed";
+}
 
-// const result = myName()
-// console.log(result);
+const result = myName();
+console.log(result);
 
 // Arrow Function
-const myName = () => "Sheikh Jabed";
-const result = myName()
-console.log(result);
+const myName2 = () => "Sheikh Jabed";
+const result2 = myName2()
+console.log(result2);
+
+
+function double(x) {
+    return x * 2;
+}
+console.log(double(25));
+
+const double2 = x => x * 2;
+console.log(double2(500));
+
+
+const add = (x, y) => {
+    const total = x + y;
+    return total
+
+}
+console.log(add(3,4));
+
+
+function sum(x = 20, y = 10) {
+    return x + y;
+}
+console.log(sum(30, 20));
+
+const sum2 = (x = 40, y = 50) => x + y;
+console.log(sum2());
