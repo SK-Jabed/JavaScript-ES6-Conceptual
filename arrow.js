de4f1704362f609e4809964b7eb5ff677dd5c170
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-29 23:09:01 +0600

    JavaScript ES6 Conceptual Repository Added

---
+++ arrow.js
@@ -0,0 +1,12 @@
+// Normal Function
+// function myName() {
+//     return "Sheikh Jabed";
+// }
+
+// const result = myName()
+// console.log(result);
+
+// Arrow Function
+const myName = () => "Sheikh Jabed";
+const result = myName()
+console.log(result);
---
+++ scope.js
@@ -0,0 +1,56 @@
+/*
+* Types of Scope in JavaScript
+* 01. Global scope
+* 02. Block scope [Only Applicable for const & let Variable]
+* 03. Function scope
+*/
+
+// Access Variable Globally
+// const x = 5;
+// console.log(x);
+
+// if(true) {
+//     console.log(x);
+// }
+
+// function show() {
+//     console.log(x);
+// }
+// show();
+
+
+//Access Variable with Block Scope
+// {
+//     let x = 5;
+//     console.log(x);
+// }
+
+// console.log(x);
+
+// const x = 7;
+
+// {
+//     const x = 10;
+//     console.log(x);
+// }
+// console.log(x);
+
+// {
+//     var x = 10;
+//     console.log(x);
+// }
+// console.log(x);
+
+// function show() {
+//     const x = 5;
+//     console.log(x);
+// }
+// show();
+// console.log(x);
+
+function show() {
+    x = 5;
+    console.log(x);
+}
+show();
+console.log(x);
