63241bc8e479fabd97073dd24242ddf677dd6172
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-30 19:46:55 +0600

    Array Spread JS File Added

---
+++ logical.js
@@ -0,0 +1,18 @@
+// Logical Operators: and, or, not ==> &&, ||, !
+
+const x = 5;
+console.log(!!x);
+
+// and [&&] ==> Left and Right
+const money = true;
+console.log(money && "I will go to institute");
+
+// or [||] ==> Left or Right
+const bus = "Bus ache";
+const rickshaw = "rickshaw ache";
+console.log(bus || rickshaw);
+
+// Ternary
+const y = 10;
+console.log(y === 5 ? "Number is 5" : "Number is not five");
+
